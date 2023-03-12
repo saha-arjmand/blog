01e875cf64704b2c640d77d31ef8586cc5de00ef
saha-arjmand <s.amirhossein.arj@gmail.com>
2023-03-12 17:13:46 +0330

    Create first model of account users

--- src/account/models.py
+++ src/account/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
-# Create your models here.
+class Account(AbstractBaseUser):
+    email                   = models.EmailField(verbose_name="email", max_length=60, unique=True)
+    username                = models.CharField(max_length=30, unique=True)
+    date_joined             = models.DateTimeField(verbose_name='date joined', auto_now_add=True)
+    last_login              = models.DateTimeField(verbose_name='last login', auto_now=True)
+    is_admin                = models.BooleanField(default=False)
+    is_active               = models.BooleanField(default=True)
+    is_staff                = models.BooleanField(default=False)
+    is_superuser            = models.BooleanField(default=False)
--- src/mysite/settings.py
+++ src/mysite/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
 
     # My apps
     'home',
+    'account',
 
     # Django apps
     'django.contrib.admin',

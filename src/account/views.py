e4c0b1f233f3a939e28e8496fc304db678ba4ee6
saha-arjmand <s.amirhossein.arj@gmail.com>
2023-03-13 13:11:19 +0330

    create account html ui

--- src/account/views.py
+++ src/account/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from account.models import Account
 
-# Create your views here.
+def account_view(request):
+
+    context = {}
+
+    accounts = Account.objects.all()
+
+    context['accounts'] = accounts
+
+    return render(request, "account/account.html", context)
--- src/mysite/urls.py
+++ src/mysite/urls.py
@@ -6,7 +6,12 @@ from question.views import (
     question_view,
 )
 
+from account.views import (
+    account_view,
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('question/', question_view),
+    path('account/', account_view),
 ]

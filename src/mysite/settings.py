8e5fdeeba1f85cbdd6615e321311ec053fa93307
saha-arjmand <s.amirhossein.arj@gmail.com>
2023-03-17 10:03:23 +0330

    add static files

--- src/mysite/settings.py
+++ src/mysite/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'question',
     'account',
     'home',
+    'blog',
 
     # Django apps
     'django.contrib.admin',
@@ -129,7 +130,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'static'), os.path.join(BASE_DIR, 'media')]
 STATIC_URL = 'static/'
+MEDIA_URL = 'media/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_cdn')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media_cdn')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- src/mysite/urls.py
+++ src/mysite/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 # Home view
 from question.views import (
@@ -44,3 +46,7 @@ urlpatterns = [
      name='password_reset_complete'),
 ]
 
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root= settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)

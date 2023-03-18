1f1397a57617e2ddd5fff33a68bcc0b073c9d0e8
saha-arjmand <s.amirhossein.arj@gmail.com>
2023-03-18 09:23:01 +0330

    add post view to home display

--- src/home/views.py
+++ src/home/views.py
@@ -1,14 +1,11 @@
 from django.shortcuts import render
-from account.models import Account
+from operator import attrgetter
+from blog.models import BlogPost
 
 def home_view(request):
 
     context = {}
-
-    accounts = Account.objects.all()
-
-    context['accounts'] = accounts
-
-    # return render(request, "account/account.html", context)
+    blog_posts = sorted(BlogPost.objects.all(), key=attrgetter('date_updated'), reverse=True)
+    context['blog_posts'] = blog_posts
 
     return render(request, "home/home.html", context)

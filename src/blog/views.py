a5f74097e0ebce91fd3d8b34117fa1a9bf6804fb
saha-arjmand <s.amirhossein.arj@gmail.com>
2023-03-18 10:09:00 +0330

    add search bar and change footer style

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.db.models import Q
 from blog.models import BlogPost
 from blog.forms import CreateBlogPostForm, UpdateBlogPostForm
 from account.models import Account
@@ -23,7 +24,6 @@ def create_blog_view(request):
 	context['form'] = form
 
 	return render(request, "blog/create_blog.html", context)
-
 
 
 def detail_blog_view(request, slug):
@@ -63,3 +63,18 @@ def edit_blog_view(request, slug):
 
 	context['form'] = form
 	return render(request, 'blog/edit_blog.html', context)
+
+
+def get_blog_queryset(query=None):
+	queryset = []
+	queries = query.split(" ") # python install 2019  = [python, install, 2019]
+	for q in queries:
+		posts = BlogPost.objects.filter(
+			Q(title__icontains=q) |
+			Q(body__icontains=q)
+		).distinct()
+
+		for post in posts:
+			queryset.append(post)
+
+	return list(set(queryset))
